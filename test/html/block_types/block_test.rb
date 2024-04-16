dc1abc976cbdf47c1ca1a8e9b8b7b89bb4a31a94
Maxime Souillat <maxime@beaucouplus.com>
2024-04-16 15:03:59 +0200

    boilerplate block test

---
+++ test/html/block_types/block_test.rb
@@ -0,0 +1,26 @@
+require "test_helper"
+
+class PortableText::Html::BlockTypes::BlockTest < Minitest::Test
+  def setup
+    @link = PortableText::MarkDefs::Link.new(
+      _key: "123",
+      _type: "link",
+      href: "http://example.com"
+    )
+
+    @required_params = {
+      key: "123",
+      type: "link"
+    }
+  end
+
+  def test_delegate_href_to_link
+    mark_def = PortableText::Html::MarkDefs::Link.new(@link, **@required_params)
+    assert_equal @link.href, mark_def.href
+  end
+
+  def test_render_creates_a_tag_with_content_and_href
+    mark_def = PortableText::Html::MarkDefs::Link.new(@link, content: "Example", **@required_params)
+    assert_equal '<a href="http://example.com">Example</a>', mark_def.render
+  end
+end
